69ecd7cbe9d7590cd46c238e0b2b65cb70badf90
Pablo <pablopukha@gmail.com>
2022-02-18 13:51:22 +0200

    added slider

---
+++ docs/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".slider").slick({autoplay:!0,autoplaySpeed:4e3,infinite:!0,fade:!0})}));
---
+++ docs/js/slider.js
@@ -0,0 +1,1 @@
+$(".slider").slick({slideToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:3e3,infinite:!0});
